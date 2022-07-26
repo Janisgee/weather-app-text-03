6c851d5bcee4dd38f0e660e34e4cfd9330a0979e
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-26 12:05:35 +0800

    Setup application layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
